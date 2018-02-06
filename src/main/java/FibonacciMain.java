6bec2ef5ce6e2312136565929c5490152a244594
wsobasik <ronin12@wp.pl>
2018-02-06 09:45:21 +0100

    init

--- src/main/java/FibonacciMain.java
+++ src/main/java/FibonacciMain.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Scanner;
 
 public class FibonacciMain {
@@ -6,10 +8,31 @@ public class FibonacciMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj liczbe:");
+        int n = scanner.nextInt();
+        System.out.println("Liczba a ciagu Fibonaciego to: " + fib(n));
+        System.out.println("Liczba ciagu Fibonacciego to: " + fibIter(n));
+    }
 
-        System.out.println("Liczba a ciagu Fibonaciego to: " + fib(scanner.nextInt()));
+    private static int fibIter(int n) {
+        int n_2 = 0;
+        int n_1 = 1;
+        int n_0 = 0;
+        int tmp = 0;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
 
 
+        for (int i = 2; i <= n; i++) {
+            n_0 = n_2 + n_1;
+            tmp = n_0;
+            n_2 = n_1;
+            n_1 = tmp;
+        }
+        return n_0;
     }
 
     private static int fib(int n) {
@@ -22,4 +45,6 @@ public class FibonacciMain {
         return fib(n - 2) + fib(n - 1);
 
     }
+
+
 }
